103f8bd73cc89efe4a74f5e79f8b994af27fe9c2
Waqas Tahir <wakaztahir@gmail.com>
2019-01-11 15:40:08 +0500

    Updates

---
+++ precache-manifest.1b43c948f1acd69497f217c8eed3fcbb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d65f6248caacaed7b4f",
+    "url": "/acadtable/static/js/runtime~main.1d65f624.js"
+  },
+  {
+    "revision": "c44474235c1ee0f4aad0",
+    "url": "/acadtable/static/js/main.c4447423.chunk.js"
+  },
+  {
+    "revision": "9e235edfc280545f36a2",
+    "url": "/acadtable/static/js/1.9e235edf.chunk.js"
+  },
+  {
+    "revision": "9018eeb1dfbdcb4ee34ff6d613812dc4",
+    "url": "/acadtable/index.html"
+  }
+];
