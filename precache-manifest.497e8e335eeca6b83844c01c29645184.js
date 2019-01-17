32c04f418758a5a25cb0a26d92bbb6e78611fe48
Waqas Tahir <wakaztahir@gmail.com>
2019-01-17 19:49:07 +0500

    Updates

---
+++ precache-manifest.497e8e335eeca6b83844c01c29645184.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d65f6248caacaed7b4f",
+    "url": "/acadtable/static/js/runtime~main.1d65f624.js"
+  },
+  {
+    "revision": "4b63a8d029f4f46f6da6",
+    "url": "/acadtable/static/js/main.4b63a8d0.chunk.js"
+  },
+  {
+    "revision": "374fed932a0e89956573",
+    "url": "/acadtable/static/js/1.374fed93.chunk.js"
+  },
+  {
+    "revision": "b9f0073969e473e8ed2cb40d015376f0",
+    "url": "/acadtable/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/acadtable/precache-manifest.520231779506383a2e9fd4ec4b15ddd4.js"
+  "/acadtable/precache-manifest.497e8e335eeca6b83844c01c29645184.js"
 );
 
 workbox.clientsClaim();
