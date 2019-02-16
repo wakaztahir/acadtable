31b22f7d666e38ea1b286e8a4a7815bde4367f8f
Waqas Tahir <wakaztahir@gmail.com>
2019-02-16 19:18:44 +0500

    downloadOption and fixes

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -5,12 +5,69 @@ import { connect } from "react-redux";
 import Welcome from "./Welcome";
 import Table from "./Table";
 
+import jsPDF from "jspdf";
+import domtoimage from "dom-to-image";
+
 import { selectCollection } from "../../actions";
 
 class Display extends Component {
   state = {
-    mode: "edit"
+    mode: "edit",
+    download: false,
+    downloadType: "pdf"
   };
+  downloadPDF() {
+    let screen = this.refs.screen;
+    let doc = new jsPDF({ orientation: "landscape" });
+    doc.fromHTML(screen);
+    doc.save("file.pdf");
+  }
+  downloadPNG() {
+    let screen = this.refs.screen;
+    domtoimage
+      .toPng(screen, { bgcolor: "#fff" })
+      .then(function(dataUrl) {
+        let link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = "file.png";
+        link.target = "_blank";
+        link.click();
+      })
+      .catch(error => {
+        console.error("Error converting to PNG Image");
+      });
+  }
+  downloadJPEG() {
+    let screen = this.refs.screen;
+    domtoimage
+      .toJpeg(screen, { bgcolor: "#fff" })
+      .then(function(dataUrl) {
+        let link = document.createElement("a");
+        link.download = "file.jpeg";
+        link.href = dataUrl;
+        link.target = "_blank";
+        link.click();
+      })
+      .catch(error => {
+        console.error("Error converting to JPEG Image");
+      });
+  }
+  componentDidUpdate() {
+    if (this.state.download) {
+      switch (this.state.downloadType) {
+        case "pdf":
+        default:
+          this.downloadPDF();
+          break;
+        case "png":
+          this.downloadPNG();
+          break;
+        case "jpeg":
+          this.downloadJPEG();
+          break;
+      }
+    }
+  }
   screen() {
     let {
       tables,
@@ -48,6 +105,41 @@ class Display extends Component {
         <div>
           <button
             onClick={() => {
+              this.setState({
+                mode: "print",
+                downloadType: "pdf",
+                download: true
+              });
+            }}
+          >
+            Download As PDF
+          </button>
+          <button
+            onClick={() => {
+              this.setState({
+                mode: "print",
+                downloadType: "png",
+                download: true
+              });
+            }}
+          >
+            Download As PNG
+          </button>
+          <button
+            onClick={() => {
+              this.setState({
+                mode: "print",
+                downloadType: "jpeg",
+                download: true
+              });
+            }}
+          >
+            Download As JPEG
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
               this.setState({ mode: "edit" });
             }}
             className={this.state.mode === "edit" ? "btn-red" : ""}
@@ -63,27 +155,29 @@ class Display extends Component {
             print
           </button>
         </div>
-        {tables.map(table => {
-          let base = objector[table.base].filter(
-            i => i.id === table.baseValue
-          )[0];
-          let rows = objector[table.rows];
-          let cols = objector[table.cols];
-          return (
-            <Table
-              id={table.id}
-              key={table.id}
-              mode={this.state.mode}
-              base={base}
-              baseBlockKey={getBlockKey(table.base)}
-              rows={rows}
-              rowsBlockKey={getBlockKey(table.rows)}
-              cols={cols}
-              colsBlockKey={getBlockKey(table.cols)}
-              blocks={blocks}
-            />
-          );
-        })}
+        <div ref="screen">
+          {tables.map(table => {
+            let base = objector[table.base].filter(
+              i => i.id === table.baseValue
+            )[0];
+            let rows = objector[table.rows];
+            let cols = objector[table.cols];
+            return (
+              <Table
+                id={table.id}
+                key={table.id}
+                mode={this.state.mode}
+                base={base}
+                baseBlockKey={getBlockKey(table.base)}
+                rows={rows}
+                rowsBlockKey={getBlockKey(table.rows)}
+                cols={cols}
+                colsBlockKey={getBlockKey(table.cols)}
+                blocks={blocks}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
