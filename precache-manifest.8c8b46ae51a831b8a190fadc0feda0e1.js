3adad069ba25a6ba0a576a02e2de2ecdda854432
Waqas Tahir <wakaztahir@gmail.com>
2019-03-15 19:54:33 +0500

    Updates

--- precache-manifest.8c8b46ae51a831b8a190fadc0feda0e1.js
+++ precache-manifest.8c8b46ae51a831b8a190fadc0feda0e1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/acadtable/static/js/runtime~main.1d65f624.js"
   },
   {
-    "revision": "b02a31a49816f268e265",
-    "url": "/acadtable/static/js/main.b02a31a4.chunk.js"
+    "revision": "558c51d9f84cc8828d0c",
+    "url": "/acadtable/static/js/main.558c51d9.chunk.js"
   },
   {
     "revision": "bfc2809aea42ba6a97db",
     "url": "/acadtable/static/js/1.bfc2809a.chunk.js"
   },
   {
-    "revision": "b02a31a49816f268e265",
+    "revision": "558c51d9f84cc8828d0c",
     "url": "/acadtable/static/css/main.ef56ad47.chunk.css"
   },
   {
-    "revision": "ea456db08560d684481daec0ef987519",
+    "revision": "b370d04c3dd3f5c9bd75917953f4c8d5",
     "url": "/acadtable/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/acadtable/precache-manifest.133e64875c14a1ba1a86492ee856ba1a.js"
+  "/acadtable/precache-manifest.8c8b46ae51a831b8a190fadc0feda0e1.js"
 );
 
 workbox.clientsClaim();
